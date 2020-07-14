bfb745062fca5ec01eb9737ca481a1251e3a444c
kennypascal <kp@kennypascal.com>
2020-07-13 22:26:26 -0400

    style adjustments

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -4,12 +4,12 @@ import { NavLink } from 'react-router-dom';
 
 const COMPONENT_NAME = 'navigation';
 const COMPONENT_NAV_LINK_NAME = 'nav-link';
-const COMPONENT_NAV_LINK_SELECTED_NAME = 'nav-link--selected';
+const COMPONENT_NAV_LINK_SELECTED_NAME = `${COMPONENT_NAV_LINK_NAME}-selected`;
 
 const Navigation: React.FunctionComponent = () => {
   return (
     <div className={COMPONENT_NAME}>
-      <ul className={`${COMPONENT_NAME}`}>
+      <ul className={`${COMPONENT_NAME}__content`}>
         <li className={COMPONENT_NAV_LINK_NAME}>
           <NavLink exact to="/" activeClassName={COMPONENT_NAV_LINK_SELECTED_NAME}>
             Home
